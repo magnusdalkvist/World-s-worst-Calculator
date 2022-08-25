2dcb839c93bfcdc89945bcf32fc3651be91f71ca
Magnus Dalkvist <magn552n@stud.kea.dk>
2022-08-25 10:45:52 +0200

    read operator

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ document.querySelector("#calculate").addEventListener("click", calculate);
 function calculate() {
   first = Number(document.querySelector("#firstnumber").value);
   second = Number(document.querySelector("#secondnumber").value);
+
+  let operator = document.querySelector("#operator").value;
+  if (operator == "add") {
+    result = first + second;
+  } else if (operator == "sub") {
+    result = first - second;
+  } else if (operator == "mul") {
+    result = first * second;
+  } else {
+    result = first / second;
+  }
 }
